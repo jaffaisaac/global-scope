246088bcab81eb398d0daf7f7f7e56ba18c32b88
jaffa Isaac <jaffaisaac4@gmail.com>
2023-04-11 22:29:04 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+// Global scope 
+// this can be accessed any where in the module
+
+var a =`Jaffa`
+
+function infor(){
+    return a;
+}
+console.log(a) //jaffa
+console.log(infor())//jaffa
+
+// for this give you all results due to global scoping 😀
